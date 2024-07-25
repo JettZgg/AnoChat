793fd4f45104874f17d5519d3b4e8404b27dbd1a
JettZgg <jett.z@outlook.com>
2024-07-25 13:51:42 +0100

    Update: app/main structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,10 +7,13 @@ from sqlalchemy.orm import Session
 from app.database import SessionLocal, engine
 from app import models, crud, schemas
 from app.core import security
+from app.api import auth, text_message
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(auth.router)
+app.include_router(text_message.router)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
@@ -43,7 +46,3 @@ def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_
 async def read_protected_route(current_user: schemas.User = Depends(get_current_user)):
     return {"msg": "You are authorized"}
 
-from app.api import auth, text_message
-
-app.include_router(auth.router)
-app.include_router(text_message.router)
